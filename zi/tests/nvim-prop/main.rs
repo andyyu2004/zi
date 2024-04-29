92b04f6ef5ec5fd8bd6382ef2d8d1718c4ea274f
Andy Yu <andyyu2004@gmail.com>
2024-04-29 20:59:39 +1200

    allow multiple undo steps in tests

--- zi/tests/nvim-prop/main.rs
+++ zi/tests/nvim-prop/main.rs
@@ -53,8 +53,7 @@ t!(I, "<ESC>", nvim_test);
 t!(I, "[WBjk]+", nvim_token_motions);
 t!(I, "[dWB]+", nvim_delete_word);
 // t!(INPUT, "[cWBjk]+<ESC>", nvim_change_operator);
-// only allowing one undo step for now as there are discrepancies that need to be investigated
-t!(I, "d([WB]|(<ESC>))+u<ESC>", nvim_undo_delete_word);
+t!(I, "d([uWB]|(<ESC>))+<ESC>", nvim_undo_delete_word);
 t!(I, "([ucdWB]|(<ESC>))+<ESC>", nvim_undo, CompareFlags::IGNORE_WHITESPACE_LINES);
 
 /// Useful to test a particular case
