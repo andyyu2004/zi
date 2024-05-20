a072472d7b8191f084a9d134a6f1596273ee028b
Andy Yu <andyyu2004@gmail.com>
2024-05-20 16:20:01 +1200

    utf16 goto_definition test

--- zi/tests/zi/lsp/definition.rs
+++ zi/tests/zi/lsp/definition.rs
@@ -2,47 +2,76 @@ use zi::Url;
 
 use super::*;
 
-#[tokio::test]
-async fn lsp_definition() -> zi::Result<()> {
-    let cx = new_cx("").await;
-
-    let path = cx.tempfile(
-        r#"abc
-def
-ghi"#,
-    )?;
+async fn setup(
+    cx: &TestContext,
+    position_encoding: lsp_types::PositionEncodingKind,
+    range: lsp_types::Range,
+    text: &str,
+) -> zi::Result<TempPath> {
+    let path = cx.tempfile(text)?;
     let uri = Url::from_file_path(&path).unwrap();
 
     cx.setup_lang_server(zi::FileType::TEXT, "test-server", (), |builder| {
         builder
-            .request::<request::Initialize, _>(|_, _| async {
-                Ok(lsp_types::InitializeResult {
-                    capabilities: lsp_types::ServerCapabilities {
-                        position_encoding: Some(lsp_types::PositionEncodingKind::UTF8),
-                        definition_provider: Some(OneOf::Left(true)),
+            .request::<request::Initialize, _>(move |_, _| {
+                let value = position_encoding.clone();
+                async move {
+                    Ok(lsp_types::InitializeResult {
+                        capabilities: lsp_types::ServerCapabilities {
+                            position_encoding: Some(value),
+                            definition_provider: Some(OneOf::Left(true)),
+                            ..Default::default()
+                        },
                         ..Default::default()
-                    },
-                    ..Default::default()
-                })
+                    })
+                }
             })
             .request::<request::GotoDefinition, _>(move |_st: &mut (), _params| {
                 let uri = uri.clone();
                 async move {
                     Ok(Some(lsp_types::GotoDefinitionResponse::Scalar(lsp_types::Location {
                         uri,
-                        range: lsp_range!(2:1..2:3),
+                        range,
                     })))
                 }
             })
     })
     .await;
 
+    Ok(path)
+}
+
+#[tokio::test]
+async fn lsp_definition_utf8() -> zi::Result<()> {
+    let cx = new_cx("").await;
+
+    let path =
+        setup(&cx, lsp_types::PositionEncodingKind::UTF8, lsp_range!(0:4..0:5), r#"ab©de"#).await?;
+
     cx.with(move |editor| {
         editor.open(path, zi::OpenFlags::ACTIVE | zi::OpenFlags::SPAWN_LANGUAGE_SERVERS)
     })
     .await?;
     cx.with(move |editor| editor.goto_definition(zi::Active)).await;
-    cx.with(|editor| assert_eq!(editor.view(zi::Active).cursor(), zi::Point::new(2, 1))).await;
+    cx.with(|editor| assert_eq!(editor.view(zi::Active).cursor(), zi::Point::new(0, 4))).await;
+
+    cx.cleanup().await;
+    Ok(())
+}
+
+#[tokio::test]
+async fn lsp_definition_utf16() -> zi::Result<()> {
+    let cx = new_cx("").await;
+
+    let path = setup(&cx, lsp_types::PositionEncodingKind::UTF16, lsp_range!(0:3..0:4), r#"ab©de"#)
+        .await?;
+
+    cx.with(move |editor| {
+        editor.open(path, zi::OpenFlags::ACTIVE | zi::OpenFlags::SPAWN_LANGUAGE_SERVERS)
+    })
+    .await?;
+    cx.with(move |editor| editor.goto_definition(zi::Active)).await;
+    cx.with(|editor| assert_eq!(editor.view(zi::Active).cursor(), zi::Point::new(0, 4))).await;
 
     cx.cleanup().await;
     Ok(())
