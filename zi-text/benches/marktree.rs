c1b8efb1fe9d79e8a09f100c81b45a9bc4e77fae
Andy Yu <andyyu2004@gmail.com>
2024-06-23 23:11:20 +1200

    MarkTree benchmark demonstrates bitmap idea works
    The benchmark won't terminate if we replace `get` impl with `self.iter().find(|item| item.id() == id)`.
    `

--- zi-text/benches/marktree.rs
+++ zi-text/benches/marktree.rs
@@ -7,12 +7,12 @@ fn main() {
 
 #[divan::bench]
 fn bench_marktree_get(bencher: Bencher<'_, '_>) {
-    let mut tree = MarkTree::<_, 8>::new(10000);
-    (0..4000).for_each(|i| tree.insert((i, i as u64)));
+    let mut tree = MarkTree::<_, 8>::new(30000);
+    (0..20000).for_each(|i| tree.insert((i, i as u64)));
     bencher.bench_local(move || {
-        (0..1000).for_each(|i| {
+        (0..10000).for_each(|i| {
             let item = tree.get(i as u64);
-            // assert_eq!(item, Some((i, i as u64)));
+            assert_eq!(item, Some((i, i as u64)));
         });
     });
 }
