e728db86c0e9d14d2592cd24b3cae62890f0fa5e
Andy Yu <andyyu2004@gmail.com>
2025-05-03 11:07:59 +1200

    fix zi set command tests

--- zi/tests/zi/api/config.rs
+++ zi/tests/zi/api/config.rs
@@ -1,4 +1,7 @@
+use core::fmt;
+
 use zi::Editor;
+use zi_test::TestContext;
 
 use crate::new;
 
@@ -26,30 +29,49 @@ async fn buffer_set_command() {
     }
 
     let cx = new("").await;
+
+    async fn check_buf<T: PartialEq + fmt::Debug + Send + Sync + 'static>(
+        cx: &TestContext,
+        cmd: &'static str,
+        expected: T,
+        f: impl Fn(&zi::buffer::Settings) -> T + Send + Sync + 'static,
+    ) {
+        cx.with(move |editor| editor.execute(cmd).unwrap()).await;
+
+        cx.with(move |editor| {
+            assert_eq!(f(buf(editor)), expected);
+        })
+        .await;
+    }
+
+    async fn check_view<T: PartialEq + fmt::Debug + Send + Sync + 'static>(
+        cx: &TestContext,
+        cmd: &'static str,
+        expected: T,
+        f: impl Fn(&zi::view::Settings) -> T + Send + Sync + 'static,
+    ) {
+        cx.with(move |editor| editor.execute(cmd).unwrap()).await;
+
+        cx.with(move |editor| {
+            assert_eq!(f(view(editor)), expected);
+        })
+        .await;
+    }
+
     cx.with(|editor| {
-        // :set tabwidth
         assert_eq!(buf(editor).tab_width, 4);
-
-        // FIXME this no longer happens synchronously so doesn't really work...
-        // Even though this command could easily be done synchronously
-        editor.execute("set tabwidth 8").unwrap();
-        assert_eq!(buf(editor).tab_width, 8);
-
-        editor.execute("set ts 4").unwrap();
-        assert_eq!(buf(editor).tab_width, 4);
-
-        editor.execute("set tabstop 6").unwrap();
-        assert_eq!(buf(editor).tab_width, 6);
-
-        // :set numberwidth
-        assert_eq!(view(editor).line_number_width, 4);
-
-        editor.execute("set numberwidth 6").unwrap();
-        assert_eq!(view(editor).line_number_width, 6);
-
-        editor.execute("set nuw 3").unwrap();
-        assert_eq!(view(editor).line_number_width, 3);
     })
     .await;
+
+    // :set tabwidth
+    check_buf(&cx, "set tabwidth 8", 8, |buf| *buf.tab_width.read()).await;
+    check_buf(&cx, "set ts 4", 4, |buf| *buf.tab_width.read()).await;
+    check_buf(&cx, "set tabstop 2", 2, |buf| *buf.tab_width.read()).await;
+
+    // :set numberwidth
+    cx.with(|editor| assert_eq!(view(editor).line_number_width, 4)).await;
+    check_view(&cx, "set numberwidth 6", 6, |view| *view.line_number_width.read()).await;
+    check_view(&cx, "set nuw 3", 3, |view| *view.line_number_width.read()).await;
+
     cx.cleanup().await;
 }
